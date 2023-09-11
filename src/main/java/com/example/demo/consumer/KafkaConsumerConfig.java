129958c301056d0ce06fbbeedf7952a4d9731cb2
yakir.zagron <yakir.zagron@algosec.com>
2023-09-11 10:17:23 +0300

    getting data from configuration file

--- src/main/java/com/example/demo/consumer/KafkaConsumerConfig.java
+++ src/main/java/com/example/demo/consumer/KafkaConsumerConfig.java
@@ -2,8 +2,10 @@ package com.example.demo.consumer;
 
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
@@ -12,12 +14,15 @@ import java.util.HashMap;
 import java.util.Map;
 
 @Configuration
+@PropertySource("classpath:application.properties")
 public class KafkaConsumerConfig {
 
+    @Value("${spring.kafka.bootstrap-servers}") // Inject the 'myapp.greeting' property
+    private String bootstrapServerConfig;
     @Bean
     public ConsumerFactory<String, String> consumerFactory() {
         Map<String, Object> configProps = new HashMap<>();
-        configProps.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        configProps.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServerConfig);
         configProps.put(ConsumerConfig.GROUP_ID_CONFIG, "my-group-id");
         configProps.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         configProps.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
